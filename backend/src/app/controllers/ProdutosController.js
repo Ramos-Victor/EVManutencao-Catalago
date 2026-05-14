162734dcaa1fa95776f48397a3b32378ed1cfe25
Ramos-Victor <vm02oliveiraramos@gmail.com>
2026-05-14 16:41:44 -0300

    feat(produtosController): Padronizando a resposta da API

--- backend/src/app/controllers/ProdutosController.js
+++ backend/src/app/controllers/ProdutosController.js
@@ -1,83 +1,105 @@
 import ProdutosRepository from "../repository/ProdutosRepository.js";
 
-class ProdutosController{
+class ProdutosController {
+  async Store(req, res) {
+    try {
+      const produto = await ProdutosRepository.create(req.body);
 
-    async Store(req, res){
-        try{
-            const produto = await ProdutosRepository.create(req.body)
+      res.status(201).json({
+        success: true,
+        message: "Produto cadastrado com sucesso!",
+        data: produto,
+      });
+    } catch (err) {
+      console.log(err);
 
-            res.status(201).json(produto)
-        } catch(err){
-            console.log(err)
-
-            res.status(500).json({
-                erro: 'Erro ao cadastrar produto',
-                mensagem: err.message
-            })
-        }
+      res.status(500).json({
+        success: false,
+        message: "Erro interno do servidor",
+        error: "INTERNAL_SERVER_ERROR",
+      });
     }
+  }
 
-    async Index(req, res){
+  async Index(req, res) {
+    try {
+      const produtos = await ProdutosRepository.findAll();
 
-        try{
-            const produtos = await ProdutosRepository.findAll()
+      res.status(200).json({
+        success: true,
+        message: "Produtos listados com sucesso!",
+        data: produtos,
+      });
+    } catch (err) {
+      console.log(err);
 
-            res.status(200).json(produtos)
-        }catch (err){
-            console.log(err)
-
-            res.status(500).json({
-                erro: 'Erro ao listar produtos',
-                mensagem: err.message
-            })
-        }
+      res.status(500).json({
+        success: false,
+        message: "Erro interno do servidor",
+        error: "INTERNAL_SERVER_ERROR",
+      });
     }
-    
-    async Show(req, res){
+  }
 
-        try{
-            const produto = await ProdutosRepository.findById(req.params.id)
+  async Show(req, res) {
+    try {
+      const produto = await ProdutosRepository.findById(req.params.id);
 
-            res.status(200).json(produto)
-        } catch(err){
-            console.log(err)
+      res.status(200).json({
+        success: true,
+        message: "Produto listado com sucesso!",
+        data: produto,
+      });
+    } catch (err) {
+      console.log(err);
 
-            res.status(500).json({
-                erro: 'Erro ao procurar produto!',
-                mensagem: err.message
-            })
-        }
+      res.status(500).json({
+        success: false,
+        message: "Erro interno do servidor",
+        error: "INTERNAL_SERVER_ERROR",
+      });
     }
+  }
 
-    async Update(req, res){
-        try{
-            const produto = await ProdutosRepository.update(req.body,req.params.id)
+  async Update(req, res) {
+    try {
+      const produto = await ProdutosRepository.update(req.body, req.params.id);
 
-            res.status(200).json(produto)
-        } catch (err){
-            console.log(err)
+      res.status(200).json({
+        success: true,
+        message: "Produto atualizado com sucesso!",
+        data: produto,
+      });
+    } catch (err) {
+      console.log(err);
 
-            res.status(500).json({
-                erro: 'Erro ao atualizar produto',
-                mensagem: err.message
-            })
-        }
+      res.status(500).json({
+        success: false,
+        message: "Erro interno do servidor",
+        error: "INTERNAL_SERVER_ERROR",
+      });
     }
+  }
 
-    async Delete(req, res){
-        try{    
-            const produto = await ProdutosRepository.delete(req.params.id)
+  async Delete(req, res) {
+    try {
+      const produto = await ProdutosRepository.delete(req.params.id);
 
-            res.status(200).json(produto)
-        } catch (err){
-            console.log(err)
+      res.status(200).json({
+        success: true,
+        message: "Produto excluido com sucesso!",
+        data: produto,
+      });
+    } catch (err) {
+      console.log(err);
 
-            res.status(500).json({
-                erro: 'Erro ao deletar produto',
-                message: err.message
-            })
-        }
+      res.status(500).json({
+        success: false,
+        message: "Erro interno do servidor",
+        error: "INTERNAL_SERVER_ERROR",
+      });
     }
+  }
 }
 
-export default new ProdutosController
+export default new ProdutosController();
