8c5a173177dae2ccd24d5280e375187588ef3a0a
Ramos-Victor <vm02oliveiraramos@gmail.com>
2026-05-15 21:29:23 -0300

    Configurando variaveis de ambiente

--- backend/src/app/database/conexao.js
+++ backend/src/app/database/conexao.js
@@ -1,35 +1,47 @@
-import mysql from "mysql"
+import mysql from "mysql2";
+import "dotenv/config";
 
 const conexao = mysql.createConnection({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: '',
-    database: 'db_evmanutencao'
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-conexao.connect()
+conexao.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar:", err);
+    return;
+  }
+
+  console.log("MySQL conectado");
+});
 
 /**
- * Executa um código sql com ou sem valores 
+ * Executa um código sql com ou sem valores
  * @param {string} sql string sql a ser executada
- * @param {array []} valores 
+ * @param {array []} valores
  * @param {string} mensagemReject mensagem exibida do reject
  * @returns objeto da Promisse
  */
-export const consulta = (sql, valores = [], mensagemReject = 'Erro na consulta') => {
-    return new Promise((resolve, reject) => {
-        conexao.query(sql, valores, (error, resultado) => {
-            if (error) {
-                return reject({
-                    mensagem: mensagemReject,
-                    erroOriginal: error
-                });
-            }
-
-            return resolve(resultado);
+export const consulta = (
+  sql,
+  valores = [],
+  mensagemReject = "Erro na consulta",
+) => {
+  return new Promise((resolve, reject) => {
+    conexao.query(sql, valores, (error, resultado) => {
+      if (error) {
+        return reject({
+          mensagem: mensagemReject,
+          erroOriginal: error,
         });
+      }
+
+      return resolve(resultado);
     });
+  });
 };
 
-export default conexao
+export default conexao;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,17 +5,17 @@ import ProdutosController from "./app/controllers/ProdutosController.js";
 const router = Router();
 
 // Rotas para Serviços
-router.post("api/servicos", ServicosController.Store);
-router.get("api/servicos", ServicosController.Index);
-router.get("api/servicos/:id", ServicosController.Show);
-router.put("api/servicos/:id", ServicosController.Update);
-router.delete("api/servicos/:id", ServicosController.Delete);
+router.post("/api/servicos", ServicosController.Store);
+router.get("/api/servicos", ServicosController.Index);
+router.get("/api/servicos/:id", ServicosController.Show);
+router.put("/api/servicos/:id", ServicosController.Update);
+router.delete("/api/servicos/:id", ServicosController.Delete);
 
 // Rotas para os Produtos
-router.post("api/produtos", ProdutosController.Store);
-router.get("api/produtos", ProdutosController.Index);
-router.get("api/produtos/:id", ProdutosController.Show);
-router.put("api/produtos/:id", ProdutosController.Update);
-router.delete("api/produtos/:id", ProdutosController.Delete);
+router.post("/api/produtos", ProdutosController.Store);
+router.get("/api/produtos", ProdutosController.Index);
+router.get("/api/produtos/:id", ProdutosController.Show);
+router.put("/api/produtos/:id", ProdutosController.Update);
+router.delete("/api/produtos/:id", ProdutosController.Delete);
 
 export default router;
